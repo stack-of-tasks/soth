008f2fe14629978e7b6313af813e8470659ee43d
Mansard <nmansard@laas.fr>
2010-07-20 11:44:35 +0200

    Compilation error while multiplying HH with a row.

--- include/soth/RotationHouseholder.hpp
+++ include/soth/RotationHouseholder.hpp
@@ -1,3 +1,8 @@
+#ifndef __SOTH_ROTATION_HOUSEHOLDER__
+#define __SOTH_ROTATION_HOUSEHOLDER__
+
+
+#include <vector>
 
 namespace soth
 {
@@ -6,60 +11,119 @@ namespace soth
   /* --- ROT HH ------------------------------------------------------------- */
   /* --- ROT HH ------------------------------------------------------------- */
 
-  class RotationHouseHolder
+  class RotationHouseholder
   {
+  public:
+    typedef VectorXd::Index Index;;
+
   protected:
     VectorXd h;
     double factor;
+    Index size;
 
   public:
-    RotationHouseHolder( void );
+    RotationHouseholder( void ) {}
+    RotationHouseholder( Index inSize )
+      : h(inSize),factor(0),size(inSize) {}
 
     template< typename VectorGen >
-      RotationHouseHolder( const VectorGen& v, const double& f )
-      : h(v.size()),factor(f)
-      {
-	for( unsigned int i=0;i<v.size();++i ) h(i)=v(i);
-      }
+    RotationHouseholder( const VectorGen& v, const double& f )
+      : h(v.size()),factor(f),size(v.size())
+    {
+      for( unsigned int i=0;i<size;++i ) h(i)=v(i);
+    }
 
-    // Init from column j of QR-issued matrix m.
-    template< typename MatrixType >
-    void init( MatrixType& m, unsigned int j );
+    template< typename MatrixGen,typename VectorGen >
+    RotationHouseholder( const MatrixGen& qr, const VectorGen& coeff, Index j )
+    {      init(qr,coeff,j);    }
+
+    // Init from column j of QR-issued matrix qr.
+    template< typename MatrixGen,typename VectorGen >
+    void init( const MatrixGen& qr, const VectorGen& coeff, Index j );
 
     /* --- operator -- */
-    template <typename MatrixGen >
-    MatrixGen & multiplyMatrixLeft( MatrixGen & m );
-    template <typename MatrixGen >
-    MatrixGen & multiplyMatrixRight( MatrixGen & m );
-    template <typename VectorGen >
-    VectorGen & multiplyVector( VectorGen & m );
 
-  };
+    // M := M*H;
+    template< typename MatrixGen >
+    MatrixGen & multiplyMatrixLeft( MatrixGen & M );
+    // M := H*M;
+    template< typename MatrixGen >
+    MatrixGen & multiplyMatrixRight( MatrixGen & M );
+    // v := H*v = v'*H
+    template< typename VectorGen >
+    void multiplyVector( VectorGen & v );
 
-  typedef std::list< RotationHouseHolder > RotationHouseHolder_list_t;
+  }; // class RotationHouseholder
+
+  typedef std::vector< RotationHouseholder > householder_sequence_t;
+
 
   /* --- HEAVY CODE --------------------------------------------------------- */
-
-  template< typename MatrixGen >
-  void initFromQR( RotationHouseHolder_list_t& hh, MatrixGen& A )
+  template< typename MatrixGen,typename VectorGen >
+  void RotationHouseholder::
+  init( const MatrixGen& qr, const VectorGen& coeff, Index j )
   {
-    const unsigned int colA=A.size2();
-    const unsigned int rowA=A.size1();
-    hh.resize(colA);
-    RotationHouseHolder_list_t::iterator iterHH=hh.begin();
-    for( unsigned int j=0;j<colA;++j,++iterHH )
-      {
-	iterHH->init(A,j);
-	for( unsigned int i=j+1;i<rowA; ++i ) { A(i,j) = 0; }
-      }
+    assert( qr.diagonalSize() == coeff.size() );
+    assert( qr.cols()>j );
+    assert( j>=0 );
+
+    Index n = qr.rows();
+    size = n-j-1;
+    h = qr.col(j).tail(n-j-1);
+    factor = coeff(j);
   }
 
-  template< typename MatrixGen >
-  void prod( const RotationHouseHolder_list_t& hh, MatrixGen& A );
-  template< typename MatrixGen >
-  void prod( MatrixGen& A,const RotationHouseHolder_list_t& hh );
+  template< typename VectorGen >
+  void RotationHouseholder::
+  multiplyVector( VectorGen & v )
+  {
+    typedef typename VectorGen::Index Index;
+    // v -= b*g*(g'*v)
+    // with g=[ 0...0 1 h ];
+    const Index nv = v.size();
+    const Index & nh = size;
+
+    // Compute g'*v
+    const Index vstart = nv-nh;
+    double gv = v(vstart-1);
+    for( Index i=0;i<nh;++i )
+      {
+	gv += v(vstart+i)*h(i);
+      }
+
+    // Compute b*g'*v
+    gv *= factor;
+
+    // Apply v -= h*bgv
+    v(vstart-1) -= gv;
+    for( Index i=0;i<nh;++i )
+      {
+	 v(vstart+i) -= gv*h(i);
+      }
+
+  }
+
+ 
+
+  /* template< typename MatrixGen > */
+  /* void initFromQR( RotationHouseholder_list_t& hh, MatrixGen& A ) */
+  /* { */
+  /*   const unsigned int colA=A.size2(); */
+  /*   const unsigned int rowA=A.size1(); */
+  /*   hh.resize(colA); */
+  /*   RotationHouseholder_list_t::iterator iterHH=hh.begin(); */
+  /*   for( unsigned int j=0;j<colA;++j,++iterHH ) */
+  /*     { */
+  /* 	iterHH->init(A,j); */
+  /* 	for( unsigned int i=j+1;i<rowA; ++i ) { A(i,j) = 0; } */
+  /*     } */
+  /* } */
 
 
 
 
 }; // namespace soth
+
+
+#endif //#ifndef __SOTH_ROTATION_HOUSEHOLDER__
+
--- unitTesting/tqr.cpp
+++ unitTesting/tqr.cpp
@@ -3,26 +3,39 @@
  */
 
 #include "soth/Algebra.h"
+#include "soth/RotationHouseholder.hpp"
 #include <iostream>
 #include <Eigen/QR>
 
 
 int main (int argc, char** argv)
 {
-  soth::MatrixXd A(4,5);
-  soth::randMatrix(A,9,5);
-  std::cout << "A = " << (soth::MATLAB)A << std::endl;
+  soth::MatrixXd J;
+  soth::randMatrix(J,5,9);
+  std::cout << "J = " << (soth::MATLAB)J << std::endl;
 
-  //Eigen::QR<Eigen::MatrixXd> Aqr(A);
-  Eigen::ColPivHouseholderQR<Eigen::MatrixXd> Aqr(5,4);
-
-  Aqr.compute(A.transpose());
-  Eigen::ColPivHouseholderQR<Eigen::MatrixXd> Aqr2(A);
+  Eigen::ColPivHouseholderQR<Eigen::MatrixXd> Aqr(9,5);
+  Aqr.compute(J.transpose());
 
   soth::MatrixXd Q = Aqr.householderQ();
   std::cout << "Q = " << (soth::MATLAB)Q << std::endl;
 
   soth::MatrixXd P = Aqr.colsPermutation();
   std::cout << "P = " << (soth::MATLAB)P << std::endl;
+
+  const Eigen::MatrixXd & mQR = Aqr.matrixQR();
+  const Eigen::VectorXd & coeff = Aqr.hCoeffs();
+
+  std::cout << "QR = " << (soth::MATLAB)mQR << std::endl;
+  std::cout << "b = " << (soth::MATLAB)coeff << std::endl;
+
+  soth::RotationHouseholder hh1;
+  hh1.init(mQR,coeff,0 );
+
+  Eigen::VectorXd v = J.row(2);
+  hh1.multiplyVector( v );
+  hh1.multiplyVector( J.row(2) );
+  std::cout << "HJ = " << (soth::MATLAB)v << std::endl;
+
 
 }
