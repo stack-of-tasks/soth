805aae68db95764ad29669c2e7275fcacbfbe9c8
Mansard <nmansard@laas.fr>
2010-07-20 10:30:24 +0200

    IVIGIT.

---
+++ include/soth/Bound.hpp
@@ -0,0 +1,36 @@
+#ifndef __SOTH_BOUND__
+#define __SOTH_BOUND__
+
+namespace soth
+{
+  class Bound
+  {
+  public:
+    enum bound_t
+      {
+	BOUND_NONE
+	,BOUND_INF
+	,BOUND_SUP
+	,BOUND_DOUBLE
+	,BOUND_TWIN // equality
+      };
+
+  protected:
+    bound_t type;
+    // In case of twin bounds, the value is stored in valInf.
+    double valInf,valSup;
+    double & valTwin;
+
+  public:
+    Bound( const double & val, bound_t type );
+    Bound( const double & inValInf, const double & inValSup );
+
+    const double& getBound( bound_t type );
+    /* Return the bound that is violated, NONE if bound are OK.
+     * In case of twin-bounds, no check is performed, NONE is always returned. */
+    bound_t check( const double & val );
+
+  }; // Class Bound
+}; // namespace soth
+
+#endif // #ifndef __SOTH_BOUND__
---
+++ src/Bound.cpp
@@ -0,0 +1,69 @@
+#include <assert.h>
+#include "soth/Bound.hpp"
+
+namespace soth
+{
+
+  Bound::Bound( const double & val, bound_t inType )
+    : type(inType),valInf(val),valSup(val),valTwin(valInf)
+  {
+    assert( inType != BOUND_DOUBLE );
+    assert( inType != BOUND_NONE );
+
+    type = inType;
+  }
+
+  Bound::Bound( const double & inValInf, const double & inValSup )
+    : type(BOUND_DOUBLE),valInf(inValInf),valSup(inValSup),valTwin(valInf)
+  {
+  }
+
+  const double& Bound::
+  getBound( bound_t inType )
+  {
+    assert( inType != BOUND_NONE );
+    assert( inType != BOUND_DOUBLE );
+
+    switch( inType )
+      {
+      case BOUND_INF:
+	assert( (type==BOUND_INF)||(type==BOUND_DOUBLE) );
+	return valInf;
+      case BOUND_SUP:
+	assert( (type==BOUND_SUP)||(type==BOUND_DOUBLE) );
+	return valSup;
+      case BOUND_TWIN:
+	assert(type==BOUND_TWIN);
+	return valTwin;
+      }
+  }
+
+  /* Return the bound that is violated, NONE if bound are OK.
+   * In case of twin-bounds, no check is performed, NONE is always returned. */
+  Bound::bound_t Bound::
+  check( const double & val )
+  {
+    assert( type!=BOUND_NONE );
+    switch( type )
+      {
+      case BOUND_INF:
+	if( val<valInf ) return BOUND_INF;
+	break;
+      case BOUND_SUP:
+	if( valSup<val ) return BOUND_SUP;
+	break;
+      case BOUND_TWIN:
+	break;
+      case BOUND_DOUBLE:
+	if( val<valInf ) return BOUND_INF;
+	if( valSup<val ) return BOUND_SUP;
+      }
+    return BOUND_NONE;
+  }
+
+
+
+
+
+
+}; // namespace soth
