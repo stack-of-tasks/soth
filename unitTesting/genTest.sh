4f720c0dd515bba39581ccd74e47070ecd4104e7
Mansard <nmansard@laas.fr>
2010-07-27 18:18:56 +0200

    IVIGIT.

---
+++ unitTesting/genTest.sh
@@ -0,0 +1,20 @@
+
+ref=0
+
+nbTest=1e3
+for i in `seq 1 $nbTest`
+do
+    ./unitTesting/texhcod &> tmp.txt
+    res=$?;
+    seed=`cat tmp.txt | grep seed`
+    if [ $res != "0" ]
+    then
+	echo $i": "$seed " ---> MISS."
+	mv tmp.txt $ref.txt
+	ref=`expr $ref + 1 `
+    else
+	echo $i": "$seed " OK."
+	rm tmp.txt
+    fi
+done
+
