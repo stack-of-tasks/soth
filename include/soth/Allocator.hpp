6978f769c9ac9b0b5df779f59ad9ccb220c10fcd
Mansard <nmansard@laas.fr>
2010-08-24 17:43:43 +0200

    IVIGIT. Long times needed.

---
+++ include/soth/Allocator.hpp
@@ -0,0 +1,33 @@
+#ifndef __SOTH_ALLOCATOR__
+#define __SOTH_ALLOCATOR__
+
+
+#include <list>
+#include <iostream>
+
+namespace soth
+{
+  class AllocatorML
+  {
+    typedef std::list<unsigned int> resource_t;
+    typedef resource_t::iterator resource_iter_t;
+
+    resource_t resource;
+    unsigned int max;
+
+  public:
+    AllocatorML( unsigned int max ) : resource(),max(max) {}
+
+    void reset();
+    void resetTop( unsigned int min );
+    unsigned int get();
+    void put( const unsigned int & token );
+    void disp( std::ostream & os ) const;
+
+    friend std::ostream& operator<<( std::ostream & os, const AllocatorML & aml );
+  };
+
+} // namespace soth
+
+
+#endif  // #ifndef __SOTH_ALLOCATOR__
